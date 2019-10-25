6f1287824f093c36f79b799ffd5cb4214d1eaed5
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-10-25 16:01:59 +0200

    Added plan.txt, removed index.html and added some output functions

---
+++ config/output.php
@@ -0,0 +1,49 @@
+<?php
+
+function output_head(string $title, string $additional_head = null)
+{
+    //set headers to NOT cache a page
+    //header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+    header("Cache-Control: private, no-cache, no-store, proxy-revalidate, no-transform");
+    header("Pragma: no-cache"); //HTTP 1.0
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+    print("<!DOCTYPE html><html lang='en'>
+      <head><meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv='Cache-control' content='no-store'>
+        <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+        <title>" . $title . "</title>
+        <link rel='stylesheet' href='/include/main.css' type='text/css'>");
+    if ($additional_head !== null)
+        print($additional_head);
+    print("</head><body>");
+}
+
+function output_end()
+{
+    print("</body></html>" . PHP_EOL);
+}
+
+
+function output_header()
+{
+    print("<div>");
+}
+
+function output_footer()
+{
+    print("</div>");
+}
+
+function dbconnectfailed(string $message)
+{
+    output_head("Database Error");
+    print("<h1>Error Connecting to the database!</h1>");
+    print("Error message: " . $message);
+    output_end();
+    exit;
+    die;
+}
+
+?>
