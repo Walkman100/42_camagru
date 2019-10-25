aff38e20daa28211956a8dfed231b952277d52bf
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-10-25 16:02:36 +0200

    Added some debug files

---
+++ debug/index.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once("../config/output.php");
+
+output_head("Debug Info", "<style>table { border-collapse: collapse; }
+      td, th { border: 1px solid #aaaaaa;
+        color: black; padding: 8px; } </style>");
+?>
+
+    <table>
+      <tr>
+        <td>Server url:</td>
+        <td><?php print($_SERVER["SERVER_NAME"]); ?></td>
+      </tr>
+      <tr>
+        <td>Server Address:</td>
+        <td><?php print($_SERVER["SERVER_ADDR"]); ?></td>
+      </tr>
+      <tr>
+        <td>Server Port:</td>
+        <td><?php print($_SERVER["SERVER_PORT"]); ?></td>
+      </tr>
+      <tr>
+        <td>Your Address:</td>
+        <td><?php print($_SERVER["REMOTE_ADDR"]); ?></td>
+      </tr>
+      <tr>
+        <td>Your Port:</td>
+        <td><?php print($_SERVER["REMOTE_PORT"]); ?></td>
+      </tr>
+      <tr>
+        <td>Server Webmaster:</td>
+        <td><?php print($_SERVER["SERVER_ADMIN"]); ?></td>
+      </tr>
+      <tr>
+        <td>Current file:</td>
+        <td><?php print($_SERVER["SCRIPT_FILENAME"]); ?></td>
+      </tr>
+    </table>
+    <br />
+    <a href="info">PHP Info</a>
+
+<?php output_end(); ?>
